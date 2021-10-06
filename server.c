8bd0448e30acceaac2dfdb9e93192ee2c887647e
aggron2002 <arun.physics500@gmail.com>
2021-10-06 21:56:56 +0530

    coding completed! testing to commence!

--- server.c
+++ server.c
@@ -16,19 +16,103 @@ struct val_req_players
 };
 
 int av_ports={{8090,1},{8092,1}};
-int sockfd;
 int flag=0;
 int len,n;
+char buffer[MAXLINE];
+int refree(char *ttt[])
+{
+  //1 for game in line
+  //0 for draw
+  //2 for player1 win
+  //3 for player2 win
+  int count=0;
+  for(int i=0;i<3;i++)
+    for(int j=0;j<strlen(ttt[i]);j++)
+      if(ttt[i][j]=='X' || ttt[i][j]=='O')count++;
+  if((ttt[0][0]=='X' && ttt[1][1]=='X' && ttt[2][2]=='X') ||
+   (ttt[0][0]=='X' && ttt[0][1]=='X' && ttt[0][2]=='X') ||
+   (ttt[1][0]=='X' && ttt[1][1]=='X' && ttt[1][2]=='X') ||
+   (ttt[2][0]=='X' && ttt[2][1]=='X' && ttt[2][2]=='X') ||
+   (ttt[0][0]=='X' && ttt[1][0]=='X' && ttt[2][0]=='X') ||
+   (ttt[0][1]=='X' && ttt[1][1]=='X' && ttt[2][1]=='X') ||
+   (ttt[0][2]=='X' && ttt[1][2]=='X' && ttt[2][2]=='X') ||
+   (ttt[0][2]=='X' && ttt[1][1]=='X' && ttt[2][0]=='X'))
+    return 2;
+    else if (
+             (ttt[0][0]=='O' && ttt[1][1]=='O' && ttt[2][2]=='O') ||
+             (ttt[0][0]=='O' && ttt[0][1]=='O' && ttt[0][2]=='O') ||
+             (ttt[1][0]=='O' && ttt[1][1]=='O' && ttt[1][2]=='O') ||
+             (ttt[2][0]=='O' && ttt[2][1]=='O' && ttt[2][2]=='O') ||
+             (ttt[0][0]=='O' && ttt[1][0]=='O' && ttt[2][0]=='O') ||
+             (ttt[0][1]=='O' && ttt[1][1]=='O' && ttt[2][1]=='O') ||
+             (ttt[0][2]=='O' && ttt[1][2]=='O' && ttt[2][2]=='O') ||
+             (ttt[0][2]=='O' && ttt[1][1]=='O' && ttt[2][0]=='O'))
+    return 3;
+    else if(count!=9)
+      return 1;
+    else return 0;
+}
+
 void tictactoe(void *arg)
 {
-  char *x[]={"_ _ _", "_ _ _", "_ _ _"};
-  
+  char *x[]={"___", "___", "___"};
+  int gamesock;
+  struct sockaddr_in servaddr;
+  if ( (gamesock = socket(AF_INET, SOCK_DGRAM, 0)) < 0 )
+  {
+       perror("socket creation failed");
+       exit(EXIT_FAILURE);
+  }
+  memset(&servaddr, 0, sizeof(servaddr));
+  struct sockaddr_in player1=arg->players[0];
+  struct sockaddr_in player2=arg->players[1];
+  servaddr.sin_family = AF_INET; // IPv4
+  servaddr.sin_addr.s_addr = INADDR_ANY;
+  servaddr.sin_port = htons(arg->porter);
+  if ( bind(gamesock, (const struct sockaddr *)&servaddr, sizeof(servaddr)) < 0 )
+  {
+    perror("bind failed");
+    exit(EXIT_FAILURE);
+  }
+  int i=0;int m,u;int result=1;
+  while((result=refree(x))==1)
+  {
+    if((i++)%2==0)
+    {
+      n=recvfrom(gamesock,(char *)buffer,MAXLINE,MSG_WAITALL,(struct sockaddr *) &player1, &len);
+      buffer[n]='\0';
+      sscanf(buffer,"%d %d",&m,&n);
+      x[m][u]='X';
+    }
+    else
+    {
+      n=recvfrom(gamesock,(char *)buffer,MAXLINE,MSG_WAITALL,(struct sockaddr *) &player2, &len);
+      buffer[n]='\0';
+      sscanf(buffer,"%d %d",&m,&n);
+      x[m][u]='O';
+    }
+  }
+  if(result==2)
+  {
+    sendto(sockfd,(const char *)"X won",strlen("X won"),MSG_CONFIRM,(const struct *)&player1,len);
+    sendto(sockfd,(const char *)"X won",strlen("X won"),MSG_CONFIRM,(const struct *)&player2,len);
+  }
+  else if(result==3)
+  {
+    sendto(sockfd,(const char *)"O won",strlen("X won"),MSG_CONFIRM,(const struct *)&player1,len);
+    sendto(sockfd,(const char *)"O won",strlen("X won"),MSG_CONFIRM,(const struct *)&player2,len);
+  }
+  else if(result==0)
+  {
+    sendto(sockfd,(const char *)"Draw",strlen("X won"),MSG_CONFIRM,(const struct *)&player1,len);
+    sendto(sockfd,(const char *)"Draw",strlen("X won"),MSG_CONFIRM,(const struct *)&player2,len);
+  }
 }
 
 void main()
 {
+  int sockfd;
   //tic tac toe
-  char buffer[MAXLINE];
   struct sockaddr_in servaddr, cliaddr,cliaddr2;
   memset(&servaddr, 0, sizeof(servaddr));
   memset(&cliaddr, 0, sizeof(cliaddr));
